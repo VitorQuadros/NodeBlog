4fec29b829c43b5824c74a706536efcd2bcca698
Vitor Quadros <viitorquadros@hotmail.com>
2021-02-18 02:58:47 -0300

    feat (categories controller): posting new categories

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,9 +1,23 @@
 const express = require('express');
 
 const router = express.Router();
+const slugify = require('slugify');
+const Category = require('./Category');
 
-router.get('/categories', (req, res) => {
-  res.send('teste');
+router.get('/admin/categories/new', (req, res) => {
+  res.render('admin/categories/new');
+});
+
+router.post('/categories/save', (req, res) => {
+  const { title } = req.body;
+  if (title !== undefined) {
+    Category.create({
+      title,
+      slug: slugify(title),
+    }).then(() => res.redirect('/'));
+  } else {
+    res.redirect('/admin/categories/new');
+  }
 });
 
 module.exports = router;
