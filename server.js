56915578cb10cf53580e097e968518d5a0921187
Vitor Quadros <viitorquadros@hotmail.com>
2021-02-18 02:05:34 -0300

    create: articles and categories controller

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const connection = require('./database/database');
+const categoriesController = require('./categories/CategoriesController');
+const articlesController = require('./articles/ArticlesController');
 
 const app = express();
 // ejs view engine
@@ -13,12 +15,16 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// database connection
 connection.authenticate()
   .then(() => {
     console.log('Conexão com o DB feita com sucesso.');
   }).catch((error) => {
     console.log(error);
   });
+
+app.use('/', categoriesController);
+app.use('/', articlesController);
 
 app.get('/', (req, res) => {
   res.render('index');
