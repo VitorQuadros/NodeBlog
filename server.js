b3c7af6530627f2adae50f72fb1bd7dfca23b7e2
Vitor Quadros <viitorquadros@hotmail.com>
2021-02-21 03:21:35 -0300

    feat (article): article route

--- server.js
+++ server.js
@@ -31,11 +31,32 @@ app.use('/', categoriesController);
 app.use('/', articlesController);
 
 app.get('/', (req, res) => {
-  Article.findAll().then((articles) => {
+  Article.findAll({
+    order: [
+      ['id', 'DESC'],
+    ],
+  }).then((articles) => {
     res.render('index', {
       articles,
     });
   });
+});
+
+app.get('/:slug', (req, res) => {
+  const { slug } = req.params;
+  Article.findOne({
+    where: {
+      slug,
+    },
+  }).then((article) => {
+    if (article != undefined) {
+      res.render('article', {
+        article,
+      });
+    } else {
+      res.redirect('/');
+    }
+  }).catch((error) => res.redirect('/'));
 });
 
 app.listen(3333, () => {
