efd8d0d0f2ca667a54f1cdb018504de0f9c496ae
Vitor Quadros <viitorquadros@hotmail.com>
2021-02-21 03:10:23 -0300

    feat (home): listing articles

--- server.js
+++ server.js
@@ -31,7 +31,11 @@ app.use('/', categoriesController);
 app.use('/', articlesController);
 
 app.get('/', (req, res) => {
-  res.render('index');
+  Article.findAll().then((articles) => {
+    res.render('index', {
+      articles,
+    });
+  });
 });
 
 app.listen(3333, () => {
