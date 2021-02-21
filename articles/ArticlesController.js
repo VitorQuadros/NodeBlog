7dc25b156222cea7aeaf0f749a9fa41d1f5b9f07
Vitor Quadros <viitorquadros@hotmail.com>
2021-02-21 02:41:30 -0300

    feat (articles db): saving

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const router = express.Router();
+const slugify = require('slugify');
 const Category = require('../categories/Category');
+const Article = require('./Article');
 
-router.get('/articles', (req, res) => {
+router.get('/admin/articles', (req, res) => {
   res.send('teste');
 });
 
@@ -13,6 +15,16 @@ router.get('/admin/articles/new', (req, res) => {
       categories,
     });
   });
+});
+
+router.post('/articles/save', (req, res) => {
+  const { title, body, category } = req.body;
+  Article.create({
+    title,
+    slug: slugify(title),
+    body,
+    categoryId: category,
+  }).then(() => res.redirect('/admin/articles'));
 });
 
 module.exports = router;
